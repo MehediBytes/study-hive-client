33a855c48a2853ab8957ec0c79fbb81631bf3075
mehedi20695 <mehedi.m000@gmail.com>
2024-12-21 22:02:55 +0600

    Add favicon,Errorpage add helemet provider for dynamic title

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <div className="text-center space-y-3 py-10">
+            <Helmet>
+                <title>Error-Page | Study-Hive</title>
+            </Helmet>
+            <h1 className="text-6xl font-bold text-red-500 mb-5">OPPS!</h1>
+            <p className="text-xl font-bold">404 - Page Not Found</p>
+            <p className="pb-3 text-md font-medium text-gray-500">The page you are looking for might have been removed <br />had its name changed or is temporarily unavailable.</p>
+            <Link to='/'>
+                <button className="btn rounded-full bg-teal-600 text-white">GO TO HOMEPAGE</button>
+            </Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import HomeLayout from '../layouts/Homelayout';
+import ErrorPage from '../pages/ErrorPage';
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <HomeLayout></HomeLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: "/",
