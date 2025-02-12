7e7b9fc6208e99f200b3738786fa0323453f7a8c
mehedi20695 <mehedi.m000@gmail.com>
2025-02-12 14:04:27 +0600

    Updating error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div className="text-center space-y-3 py-10">
+        <div className="flex flex-col justify-center items-center min-h-screen">
             <Helmet>
                 <title>Error-Page | Study-Hive</title>
             </Helmet>
             <h1 className="text-6xl font-bold text-red-500 mb-5">OPPS!</h1>
-            <p className="text-xl font-bold">404 - Page Not Found</p>
-            <p className="pb-3 text-md font-medium text-gray-500">The page you are looking for might have been removed <br />had its name changed or is temporarily unavailable.</p>
+            <p className="text-xl font-bold mb-5">404 - Page Not Found</p>
+            <p className="mb-3 text-md font-medium text-gray-500">The page you are looking for might have been removed <br />had its name changed or is temporarily unavailable.</p>
             <Link to='/'>
-                <button className="btn rounded-full bg-teal-600 text-white">GO TO HOMEPAGE</button>
+                <button className="btn rounded-full bg-green-700 text-white hover:bg-green-500">GO TO HOMEPAGE</button>
             </Link>
         </div>
     );
